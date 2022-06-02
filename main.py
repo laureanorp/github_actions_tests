c1e94bf319bd0a230dd15d62ff3efcc99642b1f4
Laureano Ruiz Pérez <laureanorp@M1-Pro-de-Laure.local>
2022-06-02 09:28:02 +0100

    comment to create conflict with working branch

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # oh no, a commit in the same line as on your working branch!
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
